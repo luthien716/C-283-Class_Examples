3e6e95d4cf9d83693454d09fb138e64c425f700d
luthien716 <luthien716@hotmail.com>
2017-07-14 20:08:09 +0800

    TryParse

--- Lecture02-Examples/Example01/Program.cs
+++ Lecture02-Examples/Example01/Program.cs
@@ -11,9 +11,12 @@ namespace Example01
         private static void Main(string[] args)
         {
             Console.Write("請輸入數字1:");
-            int number1 = int.Parse(Console.ReadLine()); //儲存變數,記得將string轉變數>>>.TryParse()
+
+            int number1 = 0;
+            int.TryParse(Console.ReadLine(), out number1); //儲存變數,記得將string轉變數>>>.TryParse()
             Console.Write("請輸入數字2:");
-            int number2 = int.Parse(Console.ReadLine()); //儲存變數,記得將string轉變數>>>.TryParse()
+            int number2 = 0;
+            int.TryParse(Console.ReadLine(), out number2); //儲存變數,記得將string轉變數>>>.TryParse()
             Console.WriteLine("{0}+{1}={2}"
                 , number1
                 , number2
